531cded849c4034c03809fa8849fbb288bff80fb
GHMarek <47536098+GHMarek@users.noreply.github.com>
2023-03-31 19:32:09 +0200

    update

--- LeetCodeLibrary/MaxIncreaseToKeepCitySkyline.cs
+++ LeetCodeLibrary/MaxIncreaseToKeepCitySkyline.cs
@@ -6,13 +6,41 @@ using System.Threading.Tasks;
 
 namespace LeetCodeLibrary
 {
-    class MaxIncreaseToKeepCitySkyline
+    public class MaxIncreaseToKeepCitySkyline
     {
         public int MaxIncreaseKeepingSkyline(int[][] grid)
         {
+            int lenInd = grid.Length - 1;
+            int resSum = 0;
+
+            for (int col = 0; col <= lenInd; col++)
+            {
+                int colMax = 0;
+
+                for (int row = 0; row <= lenInd; row++)
+                {
+                    colMax = grid[row][col] > colMax ? grid[row][col] : colMax;
+
+                }
+
+                for (int row = 0; row <= lenInd; row++)
+                {
+
+                    int localMax = Math.Min(grid[row].Max(), colMax);
+                    int localHeight = grid[row][col];
+
+                    if (localMax > localHeight)
+                    {
+                        resSum += localMax - localHeight;
+                    }
+
+                }
+
+                
+            }
 
 
-            return 0;
+            return resSum;
         }
     }
 }
